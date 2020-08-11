02fbc9fe72db4d1a77c0393785a58eb97d129271
Daniel Waiguru <danielirungu91@gmail.com>
2020-08-12 00:48:53 +0300

    add Dao functions to query Run database

--- app/src/main/java/tech/danielwaiguru/fity/database/RunDao.kt
+++ app/src/main/java/tech/danielwaiguru/fity/database/RunDao.kt
@@ -1,19 +1,40 @@
 package tech.danielwaiguru.fity.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface RunDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveRun(run: Run)
+
     @Query("SELECT * FROM runs_table")
     fun getAllRuns(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeTaken) FROM runs_table")
+    fun getTotalTime(): LiveData<Long>
+
+    @Query("SELECT SUM(distance) FROM runs_table")
+    fun getTotalDistanceCovered(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM runs_table")
+    fun getTotalCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM runs_table ORDER BY distance DESC")
+    fun getRunsByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM runs_table ORDER BY timeTaken DESC")
+    fun getRunsByTimes(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM runs_table ORDER BY averageSpeed DESC")
+    fun getRunsBySpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM RUNS_TABLE ORDER BY caloriesBurned DESC")
+    fun getRunsByCalories(): LiveData<List<Run>>
+    
     @Delete
     suspend fun deleteRun(run: Run)
+
     @Query("DELETE FROM runs_table")
     suspend fun deleteAllRuns()
 }
