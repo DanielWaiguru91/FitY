4a0e7b33a70601592ee2d04af05d0a96feb9418d
Daniel Waiguru <danielirungu91@gmail.com>
2020-08-13 10:08:24 +0300

    update ui map with running routes

--- app/src/main/java/tech/danielwaiguru/fity/ui/views/RunProgressFragment.kt
+++ app/src/main/java/tech/danielwaiguru/fity/ui/views/RunProgressFragment.kt
@@ -1,18 +1,29 @@
 package tech.danielwaiguru.fity.ui.views
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.android.synthetic.main.fragment_run_progress.*
 import tech.danielwaiguru.fity.R
+import tech.danielwaiguru.fity.common.Constants.ACTION_PAUSE
+import tech.danielwaiguru.fity.common.Constants.ACTION_START
+import tech.danielwaiguru.fity.common.gone
+import tech.danielwaiguru.fity.common.visible
 import tech.danielwaiguru.fity.service.RunningService
+import tech.danielwaiguru.fity.service.route
 
 class RunProgressFragment : Fragment() {
     private var map: GoogleMap? = null
+    private var isUserRunning = false
+    private var userRoute = mutableListOf<route>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,9 +35,73 @@ class RunProgressFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
+        initListeners()
         mapView.getMapAsync {
             this.map = it
+            drawAllRoutes()
         }
+        subscriber()
+    }
+    private fun initListeners(){
+        buttonAction.setOnClickListener { flipRunningStates() }
+    }
+    private fun drawUserRoute(){
+        if (userRoute.isNotEmpty() && userRoute.last().size > 1){
+            val preLastRoute = userRoute.last()[userRoute.last().size - 2]
+            val lastRoute = userRoute.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(Color.GREEN)
+                .width(8f)
+                .add(preLastRoute)
+                .add(lastRoute)
+            this.map?.addPolyline(polylineOptions)
+        }
+    }
+    private fun drawAllRoutes(){
+        for (route in userRoute){
+            val polylineOptions = PolylineOptions()
+                .color(Color.GREEN)
+                .width(8f)
+                .addAll(route)
+            this.map?.addPolyline(polylineOptions)
+        }
+    }
+    private fun moveCameraToUserFinishLocation() {
+        val zoomLevel = 12.0f
+        if (userRoute.isNotEmpty() && userRoute.last().isNotEmpty()){
+            this.map?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(userRoute.last().last(), zoomLevel)
+            )
+        }
+    }
+    private fun updateUi(isUserRunning: Boolean){
+        this.isUserRunning = isUserRunning
+        if (!isUserRunning){
+            buttonAction.text = getString(R.string.start_run)
+            finish.visible()
+        }
+        else {
+            buttonAction.text = getString(R.string.stop_run)
+            finish.gone()
+        }
+    }
+    private fun flipRunningStates(){
+        if (isUserRunning){
+            sendIntent(ACTION_PAUSE)
+        }
+        else {
+            sendIntent(ACTION_START)
+        }
+    }
+    private fun subscriber(){
+        RunningService.isRunning.observe(viewLifecycleOwner, Observer {
+            updateUi(it)
+        })
+        RunningService.routeCoords.observe(viewLifecycleOwner, Observer {
+            userRoute = it
+            drawUserRoute()
+            moveCameraToUserFinishLocation()
+        })
     }
     private fun sendIntent(action: String){
         Intent(requireContext(), RunningService::class.java).let {
